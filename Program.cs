51489a31d19ca2389b043379428f74450675b267
chintu-bhanderi <87899102+chintu-bhanderi@users.noreply.github.com>
2023-06-19 16:08:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,32 +13,9 @@ builder.Services.AddScoped<IAuthRepo, AuthRepo>();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<HospitalContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("HospitalDbConnection")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-/*builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opts =>
-{
-    opts.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});*/
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-/*builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Description = """Standard Authorization using Bearer scheme""",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-    });
-    c.OperationFilter<SecurityRequirementsOperationFilter>();
-});*/
 
 var app = builder.Build();
 
