147dd84864973f4be4240ee92f3e5167224c11a5
chintu-bhanderi <87899102+chintu-bhanderi@users.noreply.github.com>
2023-06-19 21:09:51 +0530

    Update DoctorsController.cs

--- Controllers/DoctorsController.cs
+++ Controllers/DoctorsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using HospitalManagementApi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HospitalManagementApi.Controllers
 {
